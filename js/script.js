c2486c9b2a17d1dbee20835e0c16527f3f255bb1
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-04-06 11:31:27 +0500

    1,2

---
+++ js/script.js
@@ -0,0 +1,10 @@
+$('.hero-slider').slick({
+    slidesToShow: 1,
+    nextArrow: '<i class="hero-slider-next fa fa-angle-right"></i>',
+    prevArrow: '<i class="hero-slider-prev fa fa-angle-left"></i>',
+    infinite: true,
+    speed: 500,
+    fade: true,
+    cssEase: 'linear',
+    dots: true,
+});
