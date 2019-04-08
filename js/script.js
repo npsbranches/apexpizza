6784318d5761e30261f2a0b0e9676f40fba16bfb
npsbranches <45974239+npsbranches@users.noreply.github.com>
2019-04-08 17:18:50 +0500

    2.0

--- js/script.js
+++ js/script.js
@@ -8,3 +8,22 @@ $('.hero-slider').slick({
     cssEase: 'linear',
     dots: true,
 });
+
+$('.sets-slider').slick({
+    doots: false,
+    infinite: true,
+    centerMode: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    nextArrow: '<i class="sets-slider-next fa fa-angle-right"></i>',
+    prevArrow: '<i class="sets-slider-prev fa fa-angle-left"></i>',
+    // autoplay: true,
+    // autoplaySpeed: 2000,
+    responsive: [{
+        breakpoint: 576,
+        settings: {
+          slidesToShow: 1,
+        }
+      }]
+  })
+  
